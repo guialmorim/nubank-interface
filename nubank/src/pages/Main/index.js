753a2c52225edc90f2719f5a36dd3ff2468d0518
Guilherme Almorim <gui.almorim@gmail.com>
2020-07-27 01:52:55 -0300

    finished styles and started animations

--- nubank/src/pages/Main/index.js
+++ nubank/src/pages/Main/index.js
@@ -11,32 +11,65 @@ import {
   Annotation,
 } from './styles';
 
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translateY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+
+  function onHandlerStateChange(event) {}
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo Disponivel</Title>
-            <Description>R$ 123.456,78</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>Transferencia de fulano as 15h horas...</Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo Disponivel</Title>
+              <Description>R$ 123.456,78</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>Transferencia de fulano as 15h horas...</Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
       <Tabs />
