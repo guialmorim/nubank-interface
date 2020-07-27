8a272afacb5db435e406ad68e405bc0a367250c0
Guilherme Almorim <gui.almorim@gmail.com>
2020-07-26 22:37:53 -0300

    created header

---
+++ nubank/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container, Top, Logo, Title} from '~/pages/Main/styles';
+import logo from '~/assets/Nubank_Logo.png';
+import Icon from "react-native-vector-icons/MaterialIcons";
+export default function Header() {
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Guilherme</Title>
+      </Top>
+      <Icon name='keyboard-arrow-down' size={20} color={#fff} />
+    </Container>
+  );
+}
--- nubank/src/index.js
+++ nubank/src/index.js
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
+import {StatusBar} from 'react-native';
 import React from 'react';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+const App = () => {
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
+    <Routes />
+  </>;
+};
 
 export default App;
